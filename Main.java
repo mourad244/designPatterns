c48fbbed3ba64c9fcb98008ee03e5c2e86079390
mourad244 <mouradvilla2@gmail.com>
2021-04-28 18:06:24 +0200

    pushing 4 design patterns

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ import iterator.BrowseHistory;
 import iterator.Iterator;
 import state.Canvas;
 import state.EraserTool;
+import strategy.BlackAndWhiteFilter;
+import strategy.ImageStorage;
+import strategy.JpegCompressor;
 
 public class Main {
   public static void main(String[] args) {
@@ -29,16 +32,17 @@ public class Main {
     // -------------------------------------
 
     // 3- ---------itterator pattern---------------
-    var history = new BrowseHistory();
-    history.push("a");
-    history.push("b");
-    history.push("c");
+    /*
+     * var history = new BrowseHistory(); history.push("a"); history.push("b");
+     * history.push("c");
+     * 
+     * Iterator iterator = history.createIterator(); while (iterator.hasNext()) {
+     * var url = iterator.current(); System.out.println(url); iterator.next(); }
+     */
 
-    Iterator iterator = history.createIterator();
-    while (iterator.hasNext()) {
-      var url = iterator.current();
-      System.out.println(url);
-      iterator.next();
-    }
+    // 4_ ------------strategy pattern-------------------
+    var imageStorage = new ImageStorage(new JpegCompressor(), new BlackAndWhiteFilter());
+
+    imageStorage.store("a");
   }
 }
---
+++ strategy/ImageStorage.java
@@ -0,0 +1,20 @@
+package strategy;
+
+public class ImageStorage {
+  private Compressor compressor;
+  private Filter filter;
+
+  public ImageStorage(Compressor compressor, Filter filter) {
+    this.compressor = compressor;
+    this.filter = filter;
+  }
+
+  public void store(String fileName) {
+    // JPEG, PNG, ...
+
+    compressor.compress(fileName);
+
+    // B&W, High Contrast
+    filter.apply(fileName);
+  }
+}
